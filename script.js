e02bd2e1db3d351c3ddf9fe0c6718141c68f46ec
belabzar khansae <smartkhansae@gmail.com>
2025-05-19 21:26:20 +0100

    add files for each card

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 /* Ce fichier est javascript */ 
- 
+ // Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".travel-card");
+  
+    cards.forEach(card => {
+      card.addEventListener("mouseenter", () => {
+        card.classList.add("highlight-card");
+      });
+  
+      card.addEventListener("mouseleave", () => {
+        card.classList.remove("highlight-card");
+      });
+    });
+  });
+  
